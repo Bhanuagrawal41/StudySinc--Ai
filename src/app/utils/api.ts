e05434c06e8988cca962ff39b146bc9623525368
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-05-23 08:00:18 +0530

    frontend deploy

--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -1,4 +1,6 @@
-const BASE = 'http://localhost:3001/api';
+// const BASE = 'http://localhost:3001/api';
+
+const BASE = `${import.meta.env.VITE_API_URL}/api`;
 
 export async function generatePlan(data: {
   subject: string;
